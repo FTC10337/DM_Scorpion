02fc178e6b714c709eca2c817368bedc19bcb0b4
Hayk <hayktsh@gmail.com>
2018-10-15 23:24:17 -0500

    cleaned code and specified code for Encoders for AutoMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -35,44 +6,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+//import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
-/**
- * This file illustrates the concept of driving a path based on encoder counts.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByTime;
- *
- *  This code ALSO requires that the drive Motors have been configured such that a positive
- *  power command moves them forwards, and causes the encoders to count UP.
- *
- *   The desired path in this example is:
- *   - Drive forward for 48 inches
- *   - Spin right for 12 Inches
- *   - Drive Backwards for 24 inches
- *   - Stop and close the claw.
- *
- *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
- *  that performs the actual movement.
- *  This methods assumes that each movement is relative to the last stopping place.
- *  There are other ways to perform encoder based moves, but this method is probably the simplest.
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@Autonomous(name="Pushbot: Auto Mode", group="DarkMatter2019")
+@Autonomous(name="Scorpion: AutoMode", group="DarkMatter2019")
 //@Disabled
-public class Autonomus extends LinearOpMode {
+public class AutoMode extends LinearOpMode {
 
     /* Declare OpMode members. */
     DriveTrain scorpion = new DriveTrain();
     private ElapsedTime     runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 7 ;    // Neverrest 20
+    static final double     COUNTS_PER_MOTOR_REV    = 7 ;    // Neverest 20
     static final double     DRIVE_GEAR_REDUCTION    = 19.2 * 72 / 48  ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (4 * COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
@@ -85,12 +29,21 @@ public class Autonomus extends LinearOpMode {
 
         scorpion.init(hardwareMap);
 
+        //setting motors to use Encoders
+        scorpion.leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        scorpion.leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        scorpion.rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        scorpion.rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        scorpion.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        scorpion.leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        scorpion.rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        scorpion.rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         // Send telemetry message to signify robot waiting;
-//        telemetry.addData("Status", "Resetting Encoders");    //
-//        telemetry.update();
+        telemetry.addData("Status", "Resetting Encoders");
 
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Path0",  "tarting at %7d :%7d :%7d :%7d",
+        telemetry.addData("Path0",  "Starting at %7d :%7d :%7d :%7d",
                 scorpion.leftFront.getCurrentPosition(),
                 scorpion.leftRear.getCurrentPosition(),
                 scorpion.rightFront.getCurrentPosition(),
@@ -146,7 +99,6 @@ public class Autonomus extends LinearOpMode {
             scorpion.rightFront.setTargetPosition(newRightTargetF);
             scorpion.rightRear.setTargetPosition(newRightTargetR);
 
-
             // Turn On RUN_TO_POSITION
             scorpion.leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             scorpion.leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -161,7 +113,6 @@ public class Autonomus extends LinearOpMode {
             scorpion.rightFront.setPower(Math.abs(speed));
             scorpion.rightRear.setPower(Math.abs(speed));
 
-
             // keep looping while we are still active, and there is time left, and both motors are running.
             // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
             // its target position, the motion will stop.  This is "safer" in the event that the robot will
@@ -173,8 +124,8 @@ public class Autonomus extends LinearOpMode {
                    (scorpion.leftFront.isBusy() && scorpion.leftRear.isBusy() && scorpion.rightFront.isBusy() && scorpion.rightRear.isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTargetF, newLeftTargetR, newRightTargetF, newRightTargetR);
-                telemetry.addData("Path2",  "Running at %7d :%7d",
+                telemetry.addData("Path1",  "Running to %7d :%7d :%7d :%7d", newLeftTargetF, newLeftTargetR, newRightTargetF, newRightTargetR);
+                telemetry.addData("Path2",  "Running at %7d :%7d :%7d :%7d",
                                             scorpion.leftFront.getCurrentPosition(),
                                             scorpion.leftRear.getCurrentPosition(),
                                             scorpion.rightFront.getCurrentPosition(),
@@ -187,7 +138,6 @@ public class Autonomus extends LinearOpMode {
             scorpion.leftRear.setPower(0);
             scorpion.rightFront.setPower(0);
             scorpion.rightRear.setPower(0);
-
 
             // Turn off RUN_TO_POSITION
             scorpion.leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
